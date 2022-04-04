ee30f73266322051626f8fbfc639ba4cb11ecf7d
Sagar Prakash Chaudhari <sagarpc426@gmail.com>
2022-04-04 20:20:42 +0530

    Demo Application for CRUD using AJAX

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A([a-z0-9_\-\.]+)@([a-z0-9\-]+\.)([a-z]{2,4}|[0-9]{1,3})\z/i
+  validates :email, presence: true, length: {maximum: 255}, 
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+end
